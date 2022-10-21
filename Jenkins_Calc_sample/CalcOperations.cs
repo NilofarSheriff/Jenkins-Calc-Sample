ec4ad9f2bbb5bb8563fa16db9828b22e3714e6e8
Nilofar <nilofarsheriff@gmail.com>
2022-10-21 09:28:34 +0530

    Add project files.

---
+++ Jenkins_Calc_sample/CalcOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jenkins_Calc_sample
+{
+    public class CalcOperations
+    {
+        public int Firstnumber { get; set; }
+        public int Secondnumber { get; set; }
+
+        public int Add()
+        {
+            return Firstnumber+Secondnumber;
+        }
+
+        public int Sub()
+        {
+            return Firstnumber - Secondnumber;
+        }
+
+        public int Multiply()
+        {
+            return Firstnumber * Secondnumber;
+        }
+    }
+}
